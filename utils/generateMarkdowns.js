a0aed4fc9a58ef7f408952f531a747208b391b87
Rosa Valdez <rosa.m42@icloud.com>
2023-11-22 16:17:52 -0600

    added functions

--- utils/generateMarkdowns.js
+++ utils/generateMarkdowns.js
@@ -1,18 +1,79 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  const licenseBadges = {
+    MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]",
+    Apache: "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]",
+    GNU: "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)]",
+    BSD: "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]",
+  };
+  return licenseBadges[license] || "";
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLinks(license) {
+  const licenseLinks={
+    MIT: "(https://opensource.org/licenses/MIT)",
+    Apache: "(https://opensource.org/licenses/Apache-2.0)",
+    GNU: "(https://www.gnu.org/licenses/gpl-3.0)",
+    BSD: "(https://opensource.org/licenses/BSD-3-Clause)",
+  };
+  return licenseLinks[license] || "";
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return ""; 
+  }
+  return `
+    ## License 
 
+      Copyright © ${license}. All rights reserved.
+    
+      Licensed under the ${license} license.
+    `;
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+
+  ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+- [Contact](#contact)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributions}
+
+  ## Tests
+  ${data.tests}
+
+
+  ${renderLicenseSection(data.license)}
+
+  ## Questions
+  For Questions go to my Repo at [https://github.com/${data.repo}/]
+
+
+  ## Contact
+  For any questions or inquiries, feel free to reach out to me at ${data.email}.
 
 `;
 }
